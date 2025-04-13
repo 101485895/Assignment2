825e240cf52615a82e900f3df9f1e317c7583cbb
Matt-ru <91402367+Matt-ru@users.noreply.github.com>
2025-04-12 23:51:43 -0400

    Deleted

--- ICE3/WebApplication1/Areas/ProjectManagement/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-namespace COMP2139_ICE.Areas.ProjectManagement.Models;
-
-public class ApplicationUser : IdentityUser
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public int UserNameHangeLimit { get; set; }
-    public byte[]? ProfilePicture { get; set; }
-}
